40e10c5a66a4b569b64ac6d25e2567ddebecdac3
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-02 19:04:36 -0300

    first commit, project created

---
+++ src/main/java/com/gitub/alcemirjunior/spiringMongoDB/SpiringMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.gitub.alcemirjunior.spiringMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiringMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpiringMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitub/alcemirjunior/spiringMongoDB/SpiringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitub.alcemirjunior.spiringMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpiringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
