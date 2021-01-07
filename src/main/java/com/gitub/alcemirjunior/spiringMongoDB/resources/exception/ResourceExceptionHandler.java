eb824f01081a84298c2ba3debd2ba9028f55645c
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-07 18:55:00 -0300

    get user by id

---
+++ src/main/java/com/gitub/alcemirjunior/spiringMongoDB/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.gitub.alcemirjunior.spiringMongoDB.resources.exception;
+
+import com.gitub.alcemirjunior.spiringMongoDB.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(
+                System.currentTimeMillis(),
+                status.value(),
+                "Não Encontrado",
+                e.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
--- src/main/java/com/gitub/alcemirjunior/spiringMongoDB/services/UserService.java
+++ src/main/java/com/gitub/alcemirjunior/spiringMongoDB/services/UserService.java
@@ -2,18 +2,27 @@ package com.gitub.alcemirjunior.spiringMongoDB.services;
 
 import com.gitub.alcemirjunior.spiringMongoDB.domain.User;
 import com.gitub.alcemirjunior.spiringMongoDB.repository.UserRepository;
+import com.gitub.alcemirjunior.spiringMongoDB.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
-    private UserRepository repo;
+    private UserRepository userRepository;
 
     public List<User> findAll(){
-        return repo.findAll();
+        return userRepository.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
 }
